e15ff3d27b5d1bafc92899a557acf7f48cea0790
AleksandrsKozaletovs <125273328+AleksandrsKozaletovs@users.noreply.github.com>
2023-02-27 15:15:17 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -8,18 +8,25 @@ import numpy
 def compute_height(n, parents):
     # Write this function
     max_height = 0
-    # Your code here
+    
     return max_height
 
 
 def main():
-choose = input()
-fileInput = input()
-If choose in "I" or choose in "i":
-i = input()
-Elif choose in "F" or choose in "f":
-
-print
+    chose = input()
+    if chose in "I" or chose in "i":
+        numberInput = input()
+        valueInput = input()
+        inputResult = compute_height(numberInput,valueInput)
+        print(inputResult)
+    elif chose in "F" or chose in "f": 
+        openFilename = input()
+        if openFilename in "a":
+            return;        
+        else:            
+            file = open(openFilename,"r")
+            fileResult = compute_height(file,1)
+            print(fileResult)
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -33,9 +40,9 @@ print
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+#sys.setrecursionlimit(10**7)  # max depth of recursion
+#threading.stack_size(2**27)   # new thread will get stack of such size
+#threading.Thread(target=main).start()
 
 if __name__ == "__main__":
     main()
