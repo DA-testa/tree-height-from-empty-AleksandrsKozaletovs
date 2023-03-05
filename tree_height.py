630790bdc09e5642f6ae2ab8ed4bfe379c782802
AleksandrsKozaletovs <125273328+AleksandrsKozaletovs@users.noreply.github.com>
2023-03-05 21:56:13 +0200

    Update tree_height.py
    Parbaudījums vai pareizi strāda kods, kas atlasa max ceļu

--- tree_height.py
+++ tree_height.py
@@ -1,14 +1,29 @@
 # python3
-
+# Aleksandrs Kozaļetovs 1.grupa 1.kurss
 import sys
 import threading
 import numpy
 
 
-def compute_height(n, parents):
+def compute_height(countOfSymbols, parentsSymbols):
     # Write this function
-    max_height = 0
+    heightsArray = numpy.zeros(countOfSymbols) #create zeros Array   
+    max_height = -1 # root
     
+    for i in range (len(parentsSymbols)): # cikls, kas iet cauri visiem simboliem 
+        element = i       
+        heights = 1
+        
+        while parentsSymbols[element] != -1:
+            if heightsArray[element] != 0:
+                heights += heightsArray[element] -1
+                break
+            heights += 1
+            element = parentsSymbols[k]
+        heightsArray[i] = heights
+        max_height = max(max_height,heightsArray[i)
+        
+   
     return max_height
 
 
@@ -42,9 +57,9 @@ def main():
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-#sys.setrecursionlimit(10**7)  # max depth of recursion
-#threading.stack_size(2**27)   # new thread will get stack of such size
-#threading.Thread(target=main).start()
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
 
 if __name__ == "__main__":
     main()
